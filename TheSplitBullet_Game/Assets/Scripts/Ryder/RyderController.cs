7dc334109501d101da878631976757e9a5c18cd7
johnshields <g00348436@gmit.ie>
2020-12-17 02:16:41 +0000

    Ryder is completely rigged up - Ryder can now move in all directions with a good glow of animations.

--- TheSplitBullet_Game/Assets/Scripts/Ryder/RyderController.cs
+++ TheSplitBullet_Game/Assets/Scripts/Ryder/RyderController.cs
@@ -11,18 +11,31 @@ namespace Ryder
 {
     public class RyderController : MonoBehaviour
     {
-        // Ryder speed
+        // Ryder stats
         private float _actionSpeed = 1;
-        private float _rotateSpeed = 1;
+        [SerializeField] public float rotateSpeed = 1.0f;
         [SerializeField] public float lowProfile = 1.5f;
         [SerializeField] public float highProfile = 4.0f;
+        [SerializeField] public float jumpLevel = 5.0f;
+        private const float RyderBase = 0.5f;
 
         // Ryder animations
         private Animator _animator;
+        
+        // waling 
         private int _isWalkingHash;
-        private int _isRunningHash;
         private int _isBackWalking;
+        private int _isWalkingLeft;
+        private int _isWalkingRight;
+        
+        // running
+        private int _isRunningHash;
         private int _isBackRunning;
+        private int _isRunningLeft;
+        private int _isRunningRight;
+        
+        // other
+        private int _isInspecting;
         
         // Ryder rotate
         private Rigidbody _bodyPhysics;
@@ -32,31 +45,52 @@ namespace Ryder
         private void Start()
         {
             _animator = GetComponent<Animator>();
+            
+            // walking
             _isWalkingHash = Animator.StringToHash("isWalking");
-            _isRunningHash = Animator.StringToHash("isRunning");
             _isBackWalking = Animator.StringToHash("isBackWalking");
+            _isWalkingLeft = Animator.StringToHash("isWalkingLeft");
+            _isWalkingRight = Animator.StringToHash("isWalkingRight");
+            
+            // running
+            _isRunningHash = Animator.StringToHash("isRunning");
             _isBackRunning = Animator.StringToHash("isBackRunning");
+            _isRunningLeft = Animator.StringToHash("isRunningLeft");
+            _isRunningRight = Animator.StringToHash("isRunningRight");
+            
+            // other
+            _isInspecting = Animator.StringToHash("isInspecting");
 
-            _bodyPhysics = GetComponent <Rigidbody>();
+            _bodyPhysics = GetComponent<Rigidbody>();
         }
+
 
         // Update is called once per frame
         private void FixedUpdate()
         {
             Walk();
+            WalkLeft();
+            WalkRight();
+            
             Run();
+            RunLeft();
+            RunRight();
+            
+            Inspect();
+            Jump();
         }
 
         private void Walk()
         {
-            float horizontalMove = Input.GetAxisRaw("Horizontal") * _actionSpeed;
-            float verticalMove = Input.GetAxisRaw("Vertical") * _actionSpeed;
-            _direction = (new Vector3(horizontalMove, 0, verticalMove) * Time.deltaTime);
+            var horizontalMove = Input.GetAxisRaw("Horizontal") * _actionSpeed / 2;
+            var verticalMove = Input.GetAxisRaw("Vertical") * _actionSpeed;
+            _bodyPhysics.velocity = _direction = 
+                (new Vector3(horizontalMove, 0, verticalMove) * Time.deltaTime);
 
-            bool forwardPressed = Input.GetKey("w");
-            bool backPressed = Input.GetKey("s");
-            bool isWalking = _animator.GetBool(_isWalkingHash);
-            bool isWalkingBack = _animator.GetBool(_isBackWalking);
+            var forwardPressed = Input.GetKey("w");
+            var backPressed = Input.GetKey("s");
+            var isWalking = _animator.GetBool(_isWalkingHash);
+            var isWalkingBack = _animator.GetBool(_isBackWalking);
 
             // Idle to Walk
             if (forwardPressed)
@@ -77,32 +111,62 @@ namespace Ryder
             {
                 _animator.SetBool(_isBackWalking, true);
             }
+
+            if (!isWalkingBack || backPressed) return;
+            Turn();
+            _animator.SetBool(_isBackWalking, false);
+        }
+        
+        private void WalkLeft()
+        {
+            var leftPressed = Input.GetKey("a");
+            var isLeftWalking = _animator.GetBool(_isWalkingLeft);
             
-            if (isWalkingBack && ! backPressed)
+            if (leftPressed)
             {
-                Turn();
-                _animator.SetBool(_isBackWalking, false);
+                _actionSpeed = _actionSpeed * lowProfile;
+                _animator.SetBool(_isWalkingLeft, true);
             }
+
+            if (!isLeftWalking || leftPressed) return;
+            _actionSpeed = 1f;
+            _animator.SetBool(_isWalkingLeft, false);
+        }
+        
+        private void WalkRight()
+        {
+            var rightPressed = Input.GetKey("d");
+            var isLeftWalking = _animator.GetBool(_isWalkingRight);
+            
+            if (rightPressed)
+            {
+                _actionSpeed = _actionSpeed * lowProfile;
+                _animator.SetBool(_isWalkingRight, true);
+            }
+
+            if (!isLeftWalking || rightPressed) return;
+            _actionSpeed = 1f;
+            _animator.SetBool(_isWalkingRight, false);
         }
 
         private void Run()
         {
-            float horizontalMove = Input.GetAxisRaw("Horizontal") * _actionSpeed;
-            float verticalMove = Input.GetAxisRaw("Vertical") * _actionSpeed;
+            var horizontalMove = Input.GetAxisRaw("Horizontal") * _actionSpeed / 8;
+            var verticalMove = Input.GetAxisRaw("Vertical") * _actionSpeed;
             transform.Translate(new Vector3(horizontalMove, 0, verticalMove) * Time.deltaTime);
 
-            bool runActive = _animator.GetBool(_isRunningHash);
-            bool backRunActive = _animator.GetBool(_isBackRunning);
-            bool forwardPressed = Input.GetKey("w");
-            bool backPressed = Input.GetKey("s");
-            bool runPressed = Input.GetKey("left shift");
-            bool backRunPressed = Input.GetKey("left shift");
+            var runActive = _animator.GetBool(_isRunningHash);
+            var backRunActive = _animator.GetBool(_isBackRunning);
+            var forwardPressed = Input.GetKey("w");
+            var backPressed = Input.GetKey("s");
+            var runPressed = Input.GetKey("left shift");
+            var backRunPressed = Input.GetKey("left shift");
 
             // Walk to Run
             if (!runActive && (forwardPressed && runPressed))
             {
                 Turn();
-                _actionSpeed = _actionSpeed * highProfile;
+                _actionSpeed *= highProfile;
                 _animator.SetBool(_isRunningHash, true);
             }
 
@@ -117,23 +181,92 @@ namespace Ryder
             {
                 _animator.SetBool(_isBackRunning, true);
             }
+
+            if (!backRunActive || (backPressed && backRunPressed)) return;
+            Turn();
+            _animator.SetBool(_isBackRunning, false);
+        }
+
+        private void RunLeft()
+        {
             
-            if (backRunActive && (!backPressed || !backRunPressed))
+            var leftRunActive = _animator.GetBool(_isRunningLeft);
+            var leftPressed = Input.GetKey("a");
+            var leftRunPressed = Input.GetKey("left shift");
+            
+            // Left Walk to Left Run
+            if (!leftRunActive && (leftPressed && leftRunPressed))
             {
-                Turn();
-                _animator.SetBool(_isBackRunning, false);
+                _actionSpeed = _actionSpeed * highProfile;
+                _animator.SetBool(_isRunningLeft, true);
+            }
+
+            if (leftRunActive && (!leftPressed || !leftRunPressed))
+            {
+                _actionSpeed = 1f;
+                _animator.SetBool(_isRunningLeft, false);
             }
         }
         
+        private void RunRight()
+        {
+            
+            var rightRunActive = _animator.GetBool(_isRunningRight);
+            var rightPressed = Input.GetKey("d");
+            var rightRunPressed = Input.GetKey("left shift");
+            
+            // Left Walk to Left Run
+            if (!rightRunActive && (rightPressed && rightRunPressed))
+            {
+                _actionSpeed = _actionSpeed * highProfile;
+                _animator.SetBool(_isRunningRight, true);
+            }
+
+            if (rightRunActive && (!rightPressed || !rightRunPressed))
+            {
+                _actionSpeed = 1f;
+                _animator.SetBool(_isRunningRight, false);
+            }
+        }
+
         private void Turn()
         {
             // rotate direction
             if (_direction !=Vector3.zero)
             {
                 transform.rotation = Quaternion.Slerp
-                    (transform.rotation, Quaternion.LookRotation(_direction), _rotateSpeed * Time.deltaTime);
+                    (transform.rotation, Quaternion.LookRotation(_direction), rotateSpeed * Time.deltaTime);
             }
             _bodyPhysics.MovePosition(transform.position + _actionSpeed * Time.deltaTime * _direction);
         }
+
+        private void Inspect()
+        {
+            var inspectPressed = Input.GetKey("i");
+            var idleInspect = Input.GetKey("left ctrl");
+            var isInspecting = _animator.GetBool(_isInspecting);
+
+            // Idle to inspect
+            if (inspectPressed && idleInspect)
+            {
+                _animator.SetBool(_isInspecting, true);
+            }
+
+            if (isInspecting && (!inspectPressed && !idleInspect))
+            {
+                _animator.SetBool(_isInspecting, false);
+            }
+        }
+        private void Jump()
+        {
+            var jump = Input.GetKey("space");
+
+            if (!jump) return;
+            // player does not jump if is not touching the ground layer
+            if (transform.position.y <=RyderBase) {
+                _bodyPhysics.AddForce(Vector3.up * jumpLevel); // jump height
+            }
+        }
+        
     }
 }
