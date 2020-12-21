b5c3ee2de8991dccb601f8747728735b23bd2c8a
johnshields <g00348436@gmit.ie>
2020-12-21 05:20:04 +0000

    old movement and animations have been scraped and new ones have been implemented. - Ryder can now idle, walk and run in all directions controlled by the mouse. The camera also follows Ryder and is controlled by the mouse

--- TheSplitBullet_Game/Assets/Scripts/Ryder/RyderController.cs
+++ TheSplitBullet_Game/Assets/Scripts/Ryder/RyderController.cs
@@ -1,29 +1,106 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ryder
 {
     public class RyderController : MonoBehaviour
     {
+        private float _currentProfile;
+        public float lowProfile = 0.02f;
+        public float highProfile = 0.06f;
+        public float rotationSpeed = 4.0f;
 
-        private float _forwardInput;
-        private float _rightInput;
-        // Start is called before the first frame update
-        void Start()
+        private Rigidbody _bodyPhysics;
+        private Animator _animator;
+        private CapsuleCollider _capsuleCollider;
+
+        public Transform cameraTransform;
+
+        private float _yaw;
+        private float _pitch;
+
+        // animation triggers
+        private int _idleActive;
+        private int _walkActiveHash;
+        private int _runActiveHash;
+
+
+        private void Start()
         {
+            _bodyPhysics = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+            _capsuleCollider = GetComponent<CapsuleCollider>();
 
+            _idleActive = Animator.StringToHash("IdleActive");
+            _walkActiveHash = Animator.StringToHash("WalkActive");
+            _runActiveHash = Animator.StringToHash("RunActive");
         }
 
-        // Update is called once per frame
-        void Update()
+        private void FixedUpdate()
         {
-            transform.Translate(new Vector3(_forwardInput, 0, _rightInput));
+            RyderMovement();
+            CameraMovement();
         }
 
-        public void ActionInput(float forward, float right)
+        private void RyderMovement()
         {
-            _forwardInput = forward;
-            _rightInput = right;
+            // move Ryder
+            var z = Input.GetAxis("Vertical") * _currentProfile;
+            var y = Input.GetAxis("Horizontal") * rotationSpeed;
+            transform.Translate(0, 0, z);
+            transform.Rotate(0, y, 0);
 
+            // Player Inputs
+            var forwardPressed = Input.GetKey("w");
+            var highProfilePressed = Input.GetKey("left shift");
+
+            if (highProfilePressed)
+            {
+                if (forwardPressed)
+                {
+                    // Run
+                    _animator.SetBool(_walkActiveHash, false);
+                    _animator.SetBool(_runActiveHash, true);
+                    _animator.SetBool(_idleActive, false);
+                }
+                else
+                {
+                    // Idle
+                    _animator.SetBool(_walkActiveHash, false);
+                    _animator.SetBool(_runActiveHash, false);
+                    _animator.SetBool(_idleActive, true);
+                }
+
+                _currentProfile = highProfile;
+            }
+            else
+            {
+                if (forwardPressed)
+                {
+                    // Walk
+                    _animator.SetBool(_walkActiveHash, true);
+                    _animator.SetBool(_runActiveHash, false);
+                    _animator.SetBool(_idleActive, false);
+                }
+                else
+                {
+                    // Idle
+                    _animator.SetBool(_walkActiveHash, false);
+                    _animator.SetBool(_runActiveHash, false);
+                    _animator.SetBool(_idleActive, true);
+                }
+
+                _currentProfile = lowProfile;
+            }
+        }
+
+        private void CameraMovement()
+        {
+            // move Camera with mouse
+            _yaw += rotationSpeed * Input.GetAxisRaw("Mouse X");
+            _pitch -= rotationSpeed * Input.GetAxisRaw("Mouse Y");
+            transform.eulerAngles = new Vector3(0, _yaw, 0);
+            cameraTransform.eulerAngles = new Vector3(_pitch, _yaw, 0);
         }
     }
 }
