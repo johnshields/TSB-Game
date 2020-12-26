568277a1220977c77fbb87306894217f7525e3a2
johnshields <g00348436@gmit.ie>
2020-12-26 19:49:37 +0000

    chapter one - part three

---
+++ TheSplitBullet_Game/Assets/Scripts/Change Scene/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Effects 
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        private static Animator _animator;
+        private static int _fadeOutHash;
+        private static int _nextSceneToLoad;
+
+        private void Start()
+        {
+            _fadeOutHash = Animator.StringToHash("FadeOut");
+            _animator = GetComponent<Animator>();
+            _nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        }
+
+        public static void FadeToScene()
+        {
+            _animator.SetTrigger(_fadeOutHash);
+        }
+
+        public static void NextScene ()
+        {
+            SceneManager.LoadScene(_nextSceneToLoad);
+        }
+    }
+}
---
+++ TheSplitBullet_Game/Assets/Scripts/Effects/WaitChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Effects;
+using UnityEngine;
+
+namespace Change_Scene
+{
+    public class WaitChangeScene : MonoBehaviour
+    {
+        public static float waitTime;
+        // Start is called before the first frame update
+        public void Start()
+        {
+            StartCoroutine(WaitCut());
+        }
+
+        private static IEnumerator WaitCut()
+        {
+            FadeMusic.FadeOutMusic();
+            SceneChanger.FadeToScene();
+            yield return new WaitForSeconds(waitTime);
+            SceneChanger.NextScene();
+        }
+    }
+}
