d2ff63b3507afd5236c9c25b1bbdf604c11a0bd6
johnshields <g00348436@gmit.ie>
2020-12-28 22:25:23 +0000

    bug fixes + chapter one part 4 is set up

---
+++ TheSplitBullet_Game/Assets/Scripts/Ryder/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Effects;
+using UnityEngine;
+
+namespace Ryder
+{
+    public class BulletScript : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            StartCoroutine(BulletChangeScene());
+        }
+
+        private static IEnumerator BulletChangeScene()
+        {
+            yield return new WaitForSeconds(10);
+            FadeMusic.FadeOutMusic();
+            SceneChanger.FadeToScene();
+            yield return new WaitForSeconds(1);
+            SceneChanger.NextScene();
+        }
+    }
+}
