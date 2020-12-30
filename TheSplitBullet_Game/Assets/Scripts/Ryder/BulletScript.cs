2b1a1ab414cc94da14a34d3e2e554a43d4e37c1b
johnshields <g00348436@gmit.ie>
2020-12-30 01:03:40 +0000

    chapter one set up

--- TheSplitBullet_Game/Assets/Scripts/Ryder/BulletScript.cs
+++ TheSplitBullet_Game/Assets/Scripts/Ryder/BulletScript.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 namespace Ryder
 {
     public class BulletScript : MonoBehaviour
-    {
+    {   public float cutTime;
         private void OnTriggerEnter(Collider other)
         {
             StartCoroutine(BulletChangeScene());
         }
 
-        private static IEnumerator BulletChangeScene()
+        private IEnumerator BulletChangeScene()
         {
-            yield return new WaitForSeconds(10);
+            yield return new WaitForSeconds(cutTime);
             FadeMusic.FadeOutMusic();
             SceneChanger.FadeToScene();
             yield return new WaitForSeconds(1);
